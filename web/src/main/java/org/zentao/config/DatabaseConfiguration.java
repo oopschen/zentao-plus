2c38d1e6a0492a8b5f9f25cac3a799548256c497
Ray <linxray@gmail.com>
2018-03-08 20:48:34 +0800

    bugfix mapper scanner

--- web/src/main/java/org/zentao/config/DatabaseConfiguration.java
+++ web/src/main/java/org/zentao/config/DatabaseConfiguration.java
@@ -2,12 +2,14 @@ package org.zentao.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import javax.sql.DataSource;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan("org.zentao.mapper")
 public class DatabaseConfiguration {
 
   @ConfigurationProperties(prefix = "ds")
--- web/src/main/java/org/zentao/controller/ReportController.java
+++ web/src/main/java/org/zentao/controller/ReportController.java
@@ -1,13 +1,19 @@
 package org.zentao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zentao.entity.gen.ZtProject;
+import org.zentao.entity.gen.ZtProjectExample;
+import org.zentao.mapper.gen.ZtProjectMapper;
 
 @Controller
 @RequestMapping("/report")
 public class ReportController {
+  @Autowired
+  private ZtProjectMapper ztProjectMapper;
 
   @GetMapping("/project/{projectID}")
   public String projectReport() {
@@ -16,6 +22,10 @@ public class ReportController {
 
   @GetMapping("/project")
   public String projectReportDefault(ModelMap modelMap) {
+    ZtProjectExample query = new ZtProjectExample();
+    query.or().andDeletedEqualTo("0");
+
+    ztProjectMapper.selectByExample(query);
     modelMap.put("word", "ray");
     return "report/project";
   }
