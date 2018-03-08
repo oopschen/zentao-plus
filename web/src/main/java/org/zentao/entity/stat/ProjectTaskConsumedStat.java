f794d819518710436d0e189c95cbc57bfeb15027
Ray <linxray@gmail.com>
2018-03-08 22:38:02 +0800

    add task state

---
+++ web/src/main/java/org/zentao/entity/stat/ProjectTaskConsumedStat.java
@@ -0,0 +1,41 @@
+package org.zentao.entity.stat;
+
+/**
+ * 任务耗时统计
+ *
+ * @author Ray(chenlei linxray @ gmail.com)
+ * @date 2018/03/08
+ * @since 1.0
+ */
+public class ProjectTaskConsumedStat {
+  private String taskTyp;
+  private Long consumedTime;
+  private Integer toltalNum;
+
+  public ProjectTaskConsumedStat() {
+  }
+
+  public String getTaskTyp() {
+    return taskTyp;
+  }
+
+  public void setTaskTyp(String taskTyp) {
+    this.taskTyp = taskTyp;
+  }
+
+  public Long getConsumedTime() {
+    return consumedTime;
+  }
+
+  public void setConsumedTime(Long consumedTime) {
+    this.consumedTime = consumedTime;
+  }
+
+  public Integer getToltalNum() {
+    return toltalNum;
+  }
+
+  public void setToltalNum(Integer toltalNum) {
+    this.toltalNum = toltalNum;
+  }
+}
